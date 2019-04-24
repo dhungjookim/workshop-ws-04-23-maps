41da9177926f808c9e3019bb9a6ae29169b75d4a
DhungJoo Kim <dhungjoo.kim.19@dartmouth.edu>
2019-04-23 22:37:27 -0400

    finished workshop w/ extra credit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.881504923f36837713522351f28b7458.js"
+  "/precache-manifest.64ebbd81ec9c8185aca9f80f21bd9c2b.js"
 );
 
 workbox.clientsClaim();
